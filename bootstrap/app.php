2264e3b3eff0d6a73c213d97bbc9439588f08362
Your Name <user@example.com>
2026-01-30 15:32:11 +0500

    Project update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
-            ForceHttps::class,
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
