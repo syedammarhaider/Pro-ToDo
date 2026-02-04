b2c860a26648d3031dafffab8b4ab2906fe4121a
Your Name <user@example.com>
2026-02-04 17:24:11 +0500

    fix vite manifest path

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/pages/{$page['component']}.jsx"])
+        @vite('resources/js/app.jsx')
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
