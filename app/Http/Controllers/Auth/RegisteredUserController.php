6fac254ecde698d34340af4176a409f8bbe1e98c
Your Name <user@example.com>
2026-02-09 15:38:34 +0500

    Project update

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('todos.index', absolute: false));
+        return redirect()->route('login')->with('success', 'Registration successful! Please login to continue.');
     }
 }
