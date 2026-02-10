832894bfe52ad9dfc96942da950a7e00f1d469d4
Your Name <user@example.com>
2026-02-10 17:18:59 +0500

    fix vite manifest path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="/css/app.css">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link rel="stylesheet" href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap">
     </head>
     <body class="font-sans antialiased">
