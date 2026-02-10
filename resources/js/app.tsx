ea6d9f02f74afb9c68257174b60dca5ea2e01a0d
Your Name <user@example.com>
2026-02-10 18:41:37 +0500

    fix vite manifest path

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -12,8 +12,8 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) =>
         resolvePageComponent(
-            `./pages/${name}.tsx`,
-            import.meta.glob('./pages/**/*.tsx'),
+            `./pages/${name}.jsx`,
+            import.meta.glob('./pages/**/*.jsx'),
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
