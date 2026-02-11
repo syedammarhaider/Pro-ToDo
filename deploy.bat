96f2a780bf394743654957b32a29ff6fb6a89655
Your Name <user@example.com>
2026-02-11 15:28:00 +0500

    fix vite manifest path

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git add .
-git commit -m "Auto deploy"
-git push origin master
-ssh -i "test key.pem" ec2-user@ammar.mi3afzal.com "cd /var/www/laravel && sudo ./deploy.sh"
-echo ✅ Auto deployed!
