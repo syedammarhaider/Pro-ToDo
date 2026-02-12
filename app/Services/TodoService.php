7138cb3f1902b0bf6728835f6cad89659f56d40e
Your Name <user@example.com>
2026-02-12 14:57:55 +0500

    fix vite manifest path

--- app/Services/TodoService.php
+++ app/Services/TodoService.php
@@ -36,7 +36,7 @@ class TodoService
             // Apply sorting
             $this->applySorting($query, $sort);
 
-            return $query->paginate(50)->withQueryString();
+            return $query->paginate(30)->withQueryString();
         });
     }
 
