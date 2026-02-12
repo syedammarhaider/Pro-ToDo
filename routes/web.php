894bea2d1ead3d115331ec7730846d80a2f314db
Your Name <user@example.com>
2026-02-12 11:07:21 +0500

    fix vite manifest path

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/todos/create', [TodoController::class, 'create'])->name('todos.create');
     Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
     Route::get('/todos/{todo}', [TodoController::class, 'show'])->name('todos.show');
+    Route::get('/todos/trashed', [TodoController::class, 'trash'])->name('todos.trashed');
+    Route::post('/todos/{todo}/restore', [TodoController::class, 'restore'])->name('todos.restore');
+    Route::delete('/todos/{todo}/force-delete', [TodoController::class, 'forceDelete'])->name('todos.force-delete');
     Route::get('/todos/{todo}/edit', [TodoController::class, 'edit'])->name('todos.edit');
     Route::patch('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
     Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
